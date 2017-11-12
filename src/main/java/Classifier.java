7e6b2372bf6f8080006d1a1771dc02c9593bb6fa
Vladislav Leonov <vlad_leonov@bk.ru>
2017-11-12 20:16:16 +0300

    Add body to Classifier's Check method

--- src/main/java/Classifier.java
+++ src/main/java/Classifier.java
@@ -3,20 +3,35 @@ import javafx.util.Pair;
 import java.util.ArrayList;
 import java.util.HashMap;
 
-public class Classifier {
+class Classifier {
 
     private double spamProbability, hamProbability;
     double probabilityBorder;
-    HashMap<Integer, Double> wordSprobabilities;
+    private HashMap<Integer, Double> wordSprobabilities;
 
     void learn(ArrayList<Message> messages) {
 
         //TODO
     }
 
-    double check(Message message) {
+    private double check(Message message) {
 
-        return 0;//TODO
+        ArrayList<Integer> words = new ArrayList<>();
+        words.addAll(message.title);
+        words.addAll(message.body);
+
+        double p1 = 1, p2 = 1, pBuffer;
+        for (int word : words) {
+
+            if (wordSprobabilities.containsKey(word)) {
+
+                pBuffer = wordSprobabilities.get(word);
+                p1 *= pBuffer;
+                p2 *= 1 - pBuffer;
+            }
+        }
+
+        return p1 / (p1 + p2);
     }
 
     ArrayList<Pair<Message, Double>> fullCheck(ArrayList<Message> messages) {
