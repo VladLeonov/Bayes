5ff7f44e209cfab3aaded42564a16f6bb388cc0a
Vladislav Leonov <vlad_leonov@bk.ru>
2017-11-12 23:53:48 +0300

    Adding code to Main

--- src/main/java/Form.java
+++ src/main/java/Form.java
@@ -1,5 +1,8 @@
+import javafx.util.Pair;
+
 import javax.swing.*;
 import java.awt.*;
+import java.util.ArrayList;
 
 public class Form  extends JFrame {
 
@@ -7,7 +10,43 @@ public class Form  extends JFrame {
 
     private Form() {
 
-        //code for loading
+        ArrayList<ArrayList<Message>> dataSet = FileReader.readAllMessages();
+        ArrayList<Message> teachSet = new ArrayList<>(), testSet = new ArrayList<>();
+        Classifier classifier = new Classifier();
+        ArrayList<Pair<Message, Double>> checkResults = new ArrayList<>();
+        ContingencyTable contingencyTable = new ContingencyTable();
+
+        for (int i = 0; i < dataSet.size(); i++) {
+
+            teachSet.clear();
+            testSet.clear();
+
+            for (int j = 0; j < dataSet.size(); j++) {
+
+                if (j != i) {
+
+                    teachSet.addAll(dataSet.get(j));
+                } else {
+
+                    testSet.addAll(dataSet.get(j));
+                }
+            }
+
+            classifier.learn(teachSet);
+
+            checkResults = classifier.fullCheck(testSet);
+
+            contingencyTable.add(new ContingencyTable(checkResults, classifier.probabilityBorder));
+        }
+
+        System.out.println("F measure = " + contingencyTable.getFMeasure());
+        System.out.println("FP part = " + contingencyTable.FP / contingencyTable.getAllCases());
+        System.out.println("Probability Border = " + classifier.probabilityBorder);
+
+        for (Pair<Message, Double> result : checkResults) {
+
+            System.out.println((result.getKey().isSpam ? "Spam | " : "Ham  | ") + result.getValue());
+        }
 
         adjustDisplay();
     }
