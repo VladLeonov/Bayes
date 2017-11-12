69ebbb4a5c6dfd94330b59d89c6e2f91da76171b
Vladislav Leonov <vlad_leonov@bk.ru>
2017-11-12 23:00:35 +0300

    Add learn method to Classifier

--- src/main/java/Classifier.java
+++ src/main/java/Classifier.java
@@ -2,16 +2,75 @@ import javafx.util.Pair;
 
 import java.util.ArrayList;
 import java.util.HashMap;
+import java.util.HashSet;
 
 class Classifier {
 
     private double spamProbability, hamProbability;
     double probabilityBorder;
-    private HashMap<Integer, Double> wordSprobabilities;
+    private HashMap<Integer, Double> wordsProbabilities = new HashMap<>();
 
     void learn(ArrayList<Message> messages) {
 
-        //TODO
+        int spamNumber = 0, hamNumber = 0;
+        HashSet<Integer> words = new HashSet<>();
+
+        for (Message message : messages) {
+
+            if (message.isSpam) {
+                spamNumber++;
+            } else {
+                hamNumber++;
+            }
+
+            words.addAll(message.title);
+            words.addAll(message.body);
+        }
+
+        spamProbability = (double) spamNumber / (spamNumber + hamNumber);
+        hamProbability = (double) hamNumber / (spamNumber + hamNumber);
+
+        HashMap<Integer, Integer> spamCounter = new HashMap<>(), hamCounter = new HashMap<>();
+
+        for (int word : words) {
+
+            spamCounter.put(word, 0);
+            hamCounter.put(word, 0);
+        }
+
+        for (Message message : messages) {
+
+            words.clear();
+            words.addAll(message.title);
+            words.addAll(message.body);
+
+            if (message.isSpam) {
+
+                for (int word : words) {
+
+                    spamCounter.put(word, spamCounter.get(word) + 1);
+                }
+            } else {
+
+                for (int word : words) {
+
+                    hamCounter.put(word, hamCounter.get(word) + 1);
+                }
+            }
+        }
+
+        wordsProbabilities.clear();
+
+        double wordSpamProbability, wordHamProbability;
+        for (int word : spamCounter.keySet()) {
+
+            wordSpamProbability = (double) spamCounter.get(word) / spamNumber;
+            wordHamProbability = (double) hamCounter.get(word) / hamNumber;
+            wordsProbabilities.put(word, wordSpamProbability * spamProbability
+                    / (wordSpamProbability * spamProbability + wordHamProbability * hamProbability));
+        }
+
+        probabilityBorder = 0.5;//TODO
     }
 
     private double check(Message message) {
@@ -23,9 +82,9 @@ class Classifier {
         double p1 = 1, p2 = 1, pBuffer;
         for (int word : words) {
 
-            if (wordSprobabilities.containsKey(word)) {
+            if (wordsProbabilities.containsKey(word)) {
 
-                pBuffer = wordSprobabilities.get(word);
+                pBuffer = wordsProbabilities.get(word);
                 p1 *= pBuffer;
                 p2 *= 1 - pBuffer;
             }
